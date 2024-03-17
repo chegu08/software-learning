6104a9343d26ed188dbf46699653a853dc2752b8
chegu08 <143737377+chegu08@users.noreply.github.com>
2024-03-18 00:57:39 +0530

    Update ex.js

--- ex.js
+++ ex.js
@@ -1,2 +1,9 @@
-
-hir
+const name="John"
+const age=20
+if(age>18){
+    console.log(`${name} is a major`);
+}
+else{
+    console.log(`${name} is a minor`);
+}
+console.log("My first code successfully created");
