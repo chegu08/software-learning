6d6ec4dbb97c776f840025acb4427490388011fb
chegu08 <143737377+chegu08@users.noreply.github.com>
2024-03-29 18:52:30 +0530

    Update and rename startpage.h to headers.h

---
+++ review/headers.h
@@ -0,0 +1,21 @@
+#ifndef START_PAGE
+#define START_PAGE
+class start_page{
+    public:
+    int flag;
+    start_page();
+};
+ class Create_Account{
+    public:
+    Create_Account();
+    int choice;
+    void Create_User();
+    void Create_designer();
+};
+class Login_Account{
+    public:
+    Login_Account();
+    private: 
+    bool check_details();
+};
+#endif
