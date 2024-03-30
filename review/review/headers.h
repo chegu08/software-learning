33ecd297eb5b4302ecf405bb276d8ce64a2bbabf
chegu08 <143737377+chegu08@users.noreply.github.com>
2024-03-30 18:44:30 +0530

    Update headers.h

--- review/review/headers.h
+++ review/review/headers.h
@@ -1,7 +1,8 @@
-
+#include "designerModule.h"
 #include "userModule.h"
 #ifndef START_PAGE
 #define START_PAGE
+extern int userCount,designerCount;
 
 class start_page{
     public:
@@ -15,16 +16,20 @@ class start_page{
     void Create_User();
     void Create_designer();
 };
-class Login_Account{
-    public:
-    Login_Account();
-    private: 
-    bool check_details();
-};
 class db{
     public:
     vector<User> allUsers;
     void insertuser(User& new_user);
+    vector<designer> allDesigners;
+    void insertDesigner(designer& new_designer);
 };
-extern db obj;
+extern db user_obj,designer_obj;
+class Login_Account:public db{
+    private: 
+    int check_user_details(string name,string password);
+    int check_designer_details(string name,string password);
+    public:
+    Login_Account();
+};
+
 #endif
