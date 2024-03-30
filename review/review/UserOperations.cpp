14d1dde33237803148ed24a0e47f914c3622c331
chegu08 <143737377+chegu08@users.noreply.github.com>
2024-03-30 18:42:38 +0530

    Update UserOperations.cpp

--- review/review/UserOperations.cpp
+++ review/review/UserOperations.cpp
@@ -4,3 +4,13 @@ using namespace std;
 void db::insertuser(User& new_user){
     allUsers.push_back(new_user);
 }
+int Login_Account::check_user_details(string Name,string pw){
+    for(User x:user_obj.allUsers){
+        //0 indiating a match
+        if(Name==x.name&&pw==x.password) return 0;
+        //1 indicating incorret password
+        else if(Name==x.name&&pw!=x.password) return 1;
+    }
+    //2 indicating the user does not exist 
+    return 2;
+}
