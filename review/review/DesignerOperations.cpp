435b22d0b823be206eb9de91b829a2ab34856043
chegu08 <143737377+chegu08@users.noreply.github.com>
2024-03-30 21:28:20 +0530

    Update DesignerOperations.cpp

--- review/review/DesignerOperations.cpp
+++ review/review/DesignerOperations.cpp
@@ -5,7 +5,7 @@ void db::insertDesigner(designer& new_designer){
     allDesigners.push_back(new_designer);
 }
 int Login_Account::check_designer_details(string Name,string pw){
-    for(User x:designer_obj.allUsers){
+    for(designer x:designer_obj.allDesigners){
         //0 indiating a match
         if(Name==x.name&&pw==x.password) return 0;
         //1 indicating incorret password
