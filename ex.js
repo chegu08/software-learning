8f62b93ed87b0733ed43e1c5d66821497c3f3530
chegu08 <143737377+chegu08@users.noreply.github.com>
2024-04-09 12:54:45 +0530

    Delete ex.js

--- ex.js
+++
@@ -1,9 +0,0 @@
-const name="John"
-const age=20
-if(age>18){
-    console.log(`${name} is a major`);
-}
-else{
-    console.log(`${name} is a minor`);
-}
-console.log("My first code successfully created");
